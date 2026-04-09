256e823452ea9217f0b7fb49a5a7185a705279ff
thomas-montout <montoutthomas@gmail.com>
2026-04-09 14:14:31 +0200

    Initial commit: Setup architecture monorepo React & Symfony

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Components/layout/Header";
+// import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="app-shell">
+      <Header />
+      {/* <HomePage /> */}
+    </div>
+  );
+}
+
+export default App;
